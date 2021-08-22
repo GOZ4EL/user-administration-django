e27041ed6fb7d4a5a2f6696a02869f6c2515d06d
goz4el <hordemzerado@gmail.com>
2021-08-21 21:08:52 -0400

    Fixing error.

--- useradmin/quickstart/views.py
+++ useradmin/quickstart/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from useradmin.quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
